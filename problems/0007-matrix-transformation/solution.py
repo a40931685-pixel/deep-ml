4548143058c0149440a6a525671f4dcfae680e58
a40931685-pixel <275788730+a40931685-pixel@users.noreply.github.com>
2026-09-24 17:33:43 +0000

    Update: Matrix Transformation

--- problems/0007-matrix-transformation/solution.py
+++ problems/0007-matrix-transformation/solution.py
@@ -6,7 +6,7 @@ def transform_matrix(A: list[list[int|float]], T: list[list[int|float]], S: list
 	t=np.array(T)
 	s=np.array(S)
 
-	if np.linalg.det(s) ==0:
+	if np.linalg.det(s) ==0 or np.linalg.det(t) ==0:
 		return -1
 	
 	transformed_matrix=np.dot(np.dot(np.linalg.inv(t),a),s).tolist()
